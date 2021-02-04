9b8b8f65d9371910321511bb166c0a1a3079e60f
mickey <77456666+mikmq@users.noreply.github.com>
2021-02-04 14:54:09 -0500

    Add files via upload

---
+++ wk4 flag/sketch.js
@@ -0,0 +1,74 @@
+
+function setup() {
+    // put setup code here
+    createCanvas(innerWidth,innerHeight);
+    background(206,206,202);
+    
+//Denmark flag
+    fill('rgb(100%,5%,15%)');
+    noStroke();
+    rect(740,150,300,200);
+    
+    fill('white');
+    rect(740,232,300,30);
+    rect(843,150,30,200);
+    
+//Congo flag
+    fill(255,224,61);
+    noStroke();
+    rect(740,370,300,200);
+    
+    fill(21,171,77);
+    triangle(740, 570, 740, 370, 930, 370);
+    
+    fill(228,20,20);
+    triangle(850, 570, 1040, 370, 1040, 570);
+  
+//Japan flag
+    fill('white');
+    noStroke();
+    rect(100,150,300,200);
+    fill('rgb(100%,5%,15%)');
+    circle(250,250,115);
+    
+//Irish flag
+    fill('white');
+    noStroke();
+    rect(420,150,300,200);
+    
+    fill(0,102,51);
+    rect(420,150,100,200);
+    
+    fill(255,128,51);
+    rect(620,150,100,200);
+
+//German flag
+    fill('rgb(100%,5%,15%)');
+    rect(100+320,370,300,200);
+    
+    fill(255, 204, 0);
+    rect(100+320,503,300,67);
+    
+     fill('black');
+    rect(100+320,370,300,67);
+    
+//Scottish flag
+     fill('white');
+    rect(100,370,300,200);
+    
+    fill(58,125,213);
+    triangle(100, 541, 100, 395, 216, 468);
+    triangle(400, 541, 400, 395, 286, 468);
+    
+    triangle(120, 370, 375, 370, 250, 455);
+    triangle(120, 370+200, 375, 370+200, 250, 455+30);
+ 
+    
+}
+
+function draw() {
+    // put drawing code here 
+    
+
+}
+
